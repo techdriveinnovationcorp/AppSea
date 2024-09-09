6bd414a9016b667b1426fca60ed429a285e6e096
techdriveinnovationcorp <techdriveinnovationcorp@gmail.com>
2024-09-09 23:13:32 +0530

    AuthGuard Added

---
+++ docs/chunk-ZRKW7TPZ.js
@@ -0,0 +1,1 @@
+var e={apiKey:"AIzaSyDO-OAtdUr1YyKTSl_-Ysgy0IiRNMgINvI",authDomain:"tic-desinger.firebaseapp.com",databaseURL:"https://tic-desinger-default-rtdb.asia-southeast1.firebasedatabase.app",projectId:"tic-desinger",storageBucket:"tic-desinger.appspot.com",messagingSenderId:"152821549306",appId:"1:152821549306:web:e6a42873b97b8722b0a143",measurementId:"G-9JDG2FP46N"};export{e as a};
